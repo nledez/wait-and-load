51452607da631af4c999e77c1bc17b5a5c7d0ee8
Nicolas Ledez <github.public@ledez.net>
2022-11-08 23:35:05 +0100

    Add consul.wait() method

--- tests/test_wait_and_load.py
+++ tests/test_wait_and_load.py
@@ -1,19 +1,82 @@
 from wait_and_load import WaitAndLoad
 
+import pytest
 import requests
 
-from unittest.mock import patch
+from unittest.mock import call, patch
 
 
-@patch('consulate.Consul', **{'return_value.catalog.nodes.return_value': '[]'})
+def catlog_nodes_empty():
+    return "[]"
+
+
+def connection_error():
+    raise requests.exceptions.ConnectionError()
+
+
+@patch(
+    "consulate.Consul", **{"return_value.catalog.nodes.side_effect": catlog_nodes_empty}
+)
 def test_consul_status_ok(mock_nodes):
     wc = WaitAndLoad()
     assert wc.consul.status() is True
 
 
-@patch('consulate.Consul',
-       **{'return_value.catalog.nodes.side_effect':
-          requests.exceptions.ConnectionError()})
+@patch(
+    "consulate.Consul", **{"return_value.catalog.nodes.side_effect": connection_error}
+)
 def test_consul_status_ko(mock_nodes):
     wc = WaitAndLoad()
     assert wc.consul.status() is False
+
+
+@patch("time.sleep", return_value=None)
+@patch(
+    "consulate.Consul", **{"return_value.catalog.nodes.side_effect": connection_error}
+)
+def test_consul_wait_ko(mock_nodes, mock_sleep):
+    wc = WaitAndLoad(sleep_time=10, wait_count=5)
+    assert wc.consul.wait() is False
+    calls = [
+        call(10),
+        call(10),
+        call(10),
+        call(10),
+        call(10),
+    ]
+    assert mock_sleep.call_count == len(calls)
+    mock_sleep.assert_has_calls(calls)
+
+
+CONSUL_CALLS = 0
+CONSUL_CALL_MAX = 0
+
+
+def one_consul_call():
+    global CONSUL_CALLS, CONSUL_CALL_MAX
+    CONSUL_CALLS += 1
+    if CONSUL_CALLS >= CONSUL_CALL_MAX:
+        return catlog_nodes_empty()
+    else:
+        return connection_error()
+
+
+@patch("time.sleep", return_value=None)
+@patch(
+    "consulate.Consul", **{"return_value.catalog.nodes.side_effect": one_consul_call}
+)
+def test_consul_wait_ok(mock_nodes, mock_sleep):
+    global CONSUL_CALLS, CONSUL_CALL_MAX
+
+    CONSUL_CALLS = 0
+    CONSUL_CALL_MAX = 3
+
+    wc = WaitAndLoad(sleep_time=42, wait_count=20)
+    assert wc.consul.wait() is True
+    calls = [
+        call(42),
+        call(42),
+        call(42),
+    ]
+    assert mock_sleep.call_count == len(calls)
+    mock_sleep.assert_has_calls(calls)
--- wait_and_load/__init__.py
+++ wait_and_load/__init__.py
@@ -1,13 +1,17 @@
 import consulate
 import requests
+import time
 
-class WaitAndLoad():
-    def __init__(self):
-        self.consul = self.Consul()
 
-    class Consul():
-        def __init__(self):
+class WaitAndLoad:
+    def __init__(self, sleep_time=1, wait_count=60):
+        self.consul = self.Consul(sleep_time, wait_count)
+
+    class Consul:
+        def __init__(self, sleep_time, wait_count):
             self.consul = consulate.Consul()
+            self.sleep_time = sleep_time
+            self.wait_count = wait_count
 
         def status(self):
             try:
@@ -15,3 +19,10 @@ class WaitAndLoad():
                 return True
             except requests.exceptions.ConnectionError:
                 return False
+
+        def wait(self):
+            for _ in range(self.wait_count):
+                time.sleep(self.sleep_time)
+                if self.status():
+                    return True
+            return False
